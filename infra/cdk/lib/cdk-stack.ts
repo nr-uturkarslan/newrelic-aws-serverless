fcf69ac08daa9527398df179d0e7268026b1cd14
uturkarslan <ugurturkarslan@gmail.com>
2022-07-20 11:41:12 +0200

    Add proxy

--- infra/cdk/lib/cdk-stack.ts
+++ infra/cdk/lib/cdk-stack.ts
@@ -2,14 +2,16 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path';
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new lambda.Function(this, "my-test-lambda", {
+    // Create Proxy Lambda
+    const proxyLambda = new lambda.Function(this, "proxy", {
       runtime: Runtime.JAVA_11,
-      code: lambda.Code.fromAsset("src/test.jar"),
+      code: lambda.Code.fromAsset(path.join(__dirname, "../../../apps/proxy/target/proxy.jar")),
       handler: "handler.LambdaHandler::handleRequest",
     });
   }
