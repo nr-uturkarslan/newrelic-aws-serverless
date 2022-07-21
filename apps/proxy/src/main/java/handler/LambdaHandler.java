9c45a2ba34cc2192b12ec1268193356741e1d99f
uturkarslan <ugurturkarslan@gmail.com>
2022-07-21 17:17:42 +0200

    Add opentracing

--- apps/proxy/src/main/java/handler/LambdaHandler.java
+++ apps/proxy/src/main/java/handler/LambdaHandler.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
+import io.opentracing.util.GlobalTracer;
 import service.Validator;
 
 public class LambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
@@ -13,7 +14,27 @@ public class LambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent
             APIGatewayProxyRequestEvent input,
             Context context
     ) {
-        var validator = new Validator(context.getLogger());
-        return validator.run(input);
+        final var tracer = GlobalTracer.get();
+
+        APIGatewayProxyResponseEvent response;
+
+        // This is an example of a custom span. `FROM Span SELECT * WHERE name='MyLambdaJavaSpan'` in New Relic will find this event.
+        var customSpan = tracer.buildSpan("MyLambdaJavaSpan").start();
+        try (var scope = tracer.activateSpan(customSpan)) {
+
+            // Here, we add a tag to our custom span
+            customSpan.setTag("lambdaCustomTagKey", "lambdaCustomAttrVal");
+
+            var validator = new Validator(context.getLogger());
+            response = validator.run(input);
+        }
+        finally {
+            customSpan.finish();
+        }
+
+        // This tag gets added to the function invocation's root span, since it's active.
+        tracer.activeSpan().setTag("lambdaCustomAttrKey", "lambdaCustomAttrVal");
+
+        return response;
     }
 }
