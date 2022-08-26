a5535539e27e8eca9fa897dc3485c3bd9b31bbc5
uturkarslan <ugurturkarslan@gmail.com>
2022-08-26 14:22:26 +0300

    Add forwarder & validator

--- infra/cdk/lib/cdk-stack.ts
+++ infra/cdk/lib/cdk-stack.ts
@@ -3,13 +3,16 @@ import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
 import * as logs from 'aws-cdk-lib/aws-logs';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as path from 'path';
 
 const S3_BUCKET_NAME = "storage";
-const LAMBDA_PROXY_NAME = "proxy";
 const LAMBDA_STORER_NAME = "storer";
+const LAMBDA_PROXY_NAME = "proxy";
+const LAMBDA_FORWARDER_NAME = "forwarder";
+const LAMBDA_VALIDATOR_NAME = "validator";
 const API_GATEWAY_NAME = "apigw";
 
 //--- Run command ---//
@@ -26,8 +29,9 @@ export class CdkStack extends Stack {
     });
 
     // Create S3 bucket
-    const bucket = new s3.Bucket(this, S3_BUCKET_NAME, {
-      removalPolicy: RemovalPolicy.RETAIN,
+    const storageBucket = new s3.Bucket(this, S3_BUCKET_NAME, {
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
     });
     
     // Get AWS account ID
@@ -62,11 +66,11 @@ export class CdkStack extends Stack {
         NEW_RELIC_DISTRIBUTED_TRACING_ENABLED: "true",
         NEW_RELIC_EXTENSION_SEND_FUNCTION_LOGS: "true",
         NEW_RELIC_SERVERLESS_MODE_ENABLED: "true",
-        S3_BUCKET_NAME: bucket.bucketName,
+        S3_BUCKET_NAME: storageBucket.bucketName,
       }
     });
 
-    bucket.grantReadWrite(storerLambda);
+    storageBucket.grantReadWrite(storerLambda);
 
     // Create Proxy Lambda
     const proxyLambda = new lambda.Function(this, LAMBDA_PROXY_NAME, {
@@ -90,11 +94,61 @@ export class CdkStack extends Stack {
 
     storerLambda.grantInvoke(proxyLambda);
 
+    // Create Validator Lambda
+    const validatorLambda = new lambda.Function(this, LAMBDA_VALIDATOR_NAME, {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      code: lambda.Code.fromAsset(path.join(__dirname, "../../../apps/validator")),
+      handler: "newrelic_lambda_wrapper.handler",
+      logRetention: logs.RetentionDays.ONE_DAY,
+      layers: [newrelicLayer],
+      initialPolicy:[newRelicLicenseKeyPolicyStatement],
+      environment: {
+        NEW_RELIC_ACCOUNT_ID: `"${newRelicAccountId}"`,
+        NEW_RELIC_TRUSTED_ACCOUNT_KEY: `"${newRelicAccountId}"`,
+        NEW_RELIC_LAMBDA_HANDLER: "lambda_function.lambda_handler",
+        NEW_RELIC_LAMBDA_EXTENSION_ENABLED: "true",
+        NEW_RELIC_DISTRIBUTED_TRACING_ENABLED: "true",
+        NEW_RELIC_EXTENSION_SEND_FUNCTION_LOGS: "true",
+        NEW_RELIC_SERVERLESS_MODE_ENABLED: "true",
+      }
+    });
+
     // Create API Gateway
     const appgw = new apigateway.RestApi(this, API_GATEWAY_NAME);
 
     // Add Proxy Lambda
     appgw.root.resourceForPath(LAMBDA_PROXY_NAME).addMethod("POST",
       new apigateway.LambdaIntegration(proxyLambda));
+
+    // Add Validator Lambda
+    appgw.root.resourceForPath(LAMBDA_VALIDATOR_NAME).addMethod("POST",
+      new apigateway.LambdaIntegration(validatorLambda));
+
+    // Create Forwarder Lambda
+    const forwarderLambda = new lambda.Function(this, LAMBDA_FORWARDER_NAME, {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      code: lambda.Code.fromAsset(path.join(__dirname, "../../../apps/forwarder")),
+      handler: "newrelic_lambda_wrapper.handler",
+      logRetention: logs.RetentionDays.ONE_DAY,
+      layers: [newrelicLayer],
+      initialPolicy:[newRelicLicenseKeyPolicyStatement],
+      environment: {
+        NEW_RELIC_ACCOUNT_ID: `"${newRelicAccountId}"`,
+        NEW_RELIC_TRUSTED_ACCOUNT_KEY: `"${newRelicAccountId}"`,
+        NEW_RELIC_LAMBDA_HANDLER: "lambda_function.lambda_handler",
+        NEW_RELIC_LAMBDA_EXTENSION_ENABLED: "true",
+        NEW_RELIC_DISTRIBUTED_TRACING_ENABLED: "true",
+        NEW_RELIC_EXTENSION_SEND_FUNCTION_LOGS: "true",
+        NEW_RELIC_SERVERLESS_MODE_ENABLED: "true",
+        LAMBDA_VALIDATOR_URL: `${appgw.url}${LAMBDA_VALIDATOR_NAME}`,
+      }
+    });
+
+    storageBucket.grantRead(forwarderLambda);
+
+    storageBucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3n.LambdaDestination(forwarderLambda),
+    );
   }
 }
